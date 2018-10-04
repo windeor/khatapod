8efac402a0630789464833c538321d4a4c74f6dc
Sagar Dhavare <windeor@gmail.com>
2018-10-04 20:44:30 +0530

    base framework

---
+++ KhataFrameworkDemo/KhataFrameworkDemo.h
@@ -0,0 +1,19 @@
+//
+//  KhataFrameworkDemo.h
+//  KhataFrameworkDemo
+//
+//  Created by Sagar  Dhavare on 04/10/18.
+//  Copyright © 2018 Sagar  Dhavare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KhataFrameworkDemo.
+FOUNDATION_EXPORT double KhataFrameworkDemoVersionNumber;
+
+//! Project version string for KhataFrameworkDemo.
+FOUNDATION_EXPORT const unsigned char KhataFrameworkDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KhataFrameworkDemo/PublicHeader.h>
+
+
---
+++ KhataFrameworkDemo/Service.swift
@@ -0,0 +1,18 @@
+//  Service.swift
+//  KhataRegistration
+//
+//  Created by Puli Chakali on 04/10/18.
+//  Copyright © 2018 ANC. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){}
+    
+    public static func doSomething() -> String {
+        
+        return "test framewok"
+    }
+}
