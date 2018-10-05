d64defdcbdc18ad3179cf73a534db440a3aa14ba
Sagar Dhavare <windeor@gmail.com>
2018-10-05 10:30:48 +0530

    added viewcontroller and delegate methods

--- KhataFrameworkDemo/Sources/AadharNumberViewController.swift
+++ KhataFrameworkDemo/Sources/AadharNumberViewController.swift
@@ -8,23 +8,35 @@
 
 import UIKit
 
-class AadharNumberViewController: UIViewController {
-
-    override func viewDidLoad() {
+open class AadharNumberViewController: UIViewController {
+    
+    
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @IBOutlet weak var aadharNumberTextFeild: UITextField!
+    @IBOutlet weak var aadharNumberErrorLabel: UILabel!
+    public var khataAppDelegate:KhataAppDelegate?
+    
+    override open func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func sendData(_ sender: UIButton) {
+        if(aadharNumberTextFeild.text == ""){
+            aadharNumberErrorLabel.isHidden = false
+        }else{
+            khataAppDelegate?.reponseData(response: aadharNumberTextFeild.text!)
+        }
     }
-    */
+    
+}
 
+public protocol KhataAppDelegate {
+    
+    func reponseData(response:String)
 }
--- khataframework.podspec
+++ khataframework.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
  s.name         = "khataframework"
- s.version      = "1.0.0"
+ s.version      = "1.0.1"
  s.summary      = "A short description of DemoFramework."
  s.description  = "A short description of DemoFramework"
 
@@ -14,7 +14,7 @@ Pod::Spec.new do |s|
  s.platform     = :ios, "10.0"
 
 
- s.source        = { :git => 'https://github.com/windeor/khatapod.git',:branch => "master" }
+ s.source        = { :git => 'https://github.com/windeor/khatapod.git',:tag => "1.0.1" }
  s.swift_version = "4.0"
  s.source_files = 'KhataFrameworkDemo/**/*.{h,m,swift}'
  
